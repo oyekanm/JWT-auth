b65565b1bd729d5182865fd02a1723bbee27e4be
Oyekanmi Boluwatife <enitanboluwatife5@gmail.com>
2023-08-02 22:18:52 +0100

    changed server config

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import path from 'path';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
+
 import userRouter from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import mongooseConnect from "./config/db.js";
@@ -18,6 +20,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRouter);
+
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
+  // console.log(path.join(__dirname, 'frontend/dist'));
+  
+  app.get('*', (req, res) =>
+  // console.log(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
